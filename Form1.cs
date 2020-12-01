95be969f573640e9342b124e9fb23a80007bc169
Luis <info@lemaluis.com>
2020-11-30 23:16:03 -0500

    Initial emition.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+			locationTextBox.Text = assembly.Location;
+            versionTextBox.Text = assembly.GetName().Version.ToString(3);
+		}
+
+        private void btn_HiButton_Click(object sender, EventArgs e)
+        {
+			MessageBox.Show("Hi this is a new version of this application.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Squirrel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static async Task Main()
+        {
+
+            using (var mgr = new UpdateManager("D:\\SourceRepo\\MyApp-squirrel-test\\Releases"))
+            {
+                await mgr.UpdateApp();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
